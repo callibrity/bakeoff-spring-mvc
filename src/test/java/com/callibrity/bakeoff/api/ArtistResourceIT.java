d5fbb3d861cfe0e68684d7a5dab8a3a5d7daea96
James Carman <jcarman@callibrity.com>
2022-10-13 14:22:42 -0400

    Adding integration test.

--- src/test/java/com/callibrity/bakeoff/api/ArtistResourceIT.java
+++ src/test/java/com/callibrity/bakeoff/api/ArtistResourceIT.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -44,5 +45,28 @@ class ArtistResourceIT {
         mockMvc.perform(get("/api/artists/{id}", artist.getId()))
                 .andExpect(status().isOk());
     }
+
+    @Test
+    void shouldDeleteArtist() throws Exception {
+        CreateArtistRequest request = new CreateArtistRequest("Prince", Genre.Rock);
+        MvcResult result = mockMvc.perform(
+                        post("/api/artists")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Artist artist = mapper.readValue(result.getResponse().getContentAsString(), Artist.class);
+
+        mockMvc.perform(get("/api/artists/{id}", artist.getId()))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(delete("/api/artists/{id}", artist.getId()))
+                        .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/artists/{id}", artist.getId()))
+                .andExpect(status().isNotFound());
+    }
+
 
 }
